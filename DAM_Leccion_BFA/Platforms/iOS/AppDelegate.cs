129f260d88a7cd45aa6ae1be25067eecbe950e49
brianflowers10 <21030214@itesa.edu.mx>
2025-01-28 17:49:19 -0600

    Version_01:Se creo el proyecto MAUI vacio

---
+++ DAM_Leccion_BFA/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DAM_Leccion_BFA
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
